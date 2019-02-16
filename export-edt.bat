38e125e04497244b0fb4e97bdf5a2ec0e26c2a78
Stepa86 <Stepa86@gmail.com>
2019-02-16 16:45:33 +0300

    Чтение результатов в таблицу

--- export-edt.bat
+++ export-edt.bat
@@ -6,9 +6,9 @@ set EDT_PROJECT_VERSION=8.3.12
 set EDT_VERSION=1.10
 
 set WORKSPACE=%~dp0workspace
-set SRC_FOLDER=%~dp0src
+set SRC=%~dp0src
 set PROJECT_PATH=%~dp0edt-project
-set VALIDATION_RESULT=%~dp0edt-result.out
+set EDT_VALIDATION_RESULT=%~dp0edt-result.out
 set PLATFORM=c:\Program Files\1cv8\%VERSION%\bin\1cv8.exe
 
 set RING_OPTS=-Dfile.encoding=UTF-8 -Dosgi.nl=ru
@@ -16,11 +16,11 @@ set RING_OPTS=-Dfile.encoding=UTF-8 -Dosgi.nl=ru
 rd /S /Q "%WORKSPACE%"
 md  "%WORKSPACE%"
 rd /S /Q "%PROJECT_PATH%"
-DEL "%VALIDATION_RESULT%"
+DEL "%EDT_VALIDATION_RESULT%"
 
 ECHO Создание проекта в EDT %date% - %time%
 
-call ring edt@%EDT_VERSION% workspace import --workspace-location "%WORKSPACE%" --configuration-files "%SRC_FOLDER%" --project "%PROJECT_PATH%" --version %EDT_PROJECT_VERSION%
+call ring edt@%EDT_VERSION% workspace import --workspace-location "%WORKSPACE%" --configuration-files "%SRC%" --project "%PROJECT_PATH%" --version %EDT_PROJECT_VERSION%
 
 ECHO Проект в EDT создан %date% - %time%
 ECHO Error level: %ERRORLEVEL% 
@@ -29,6 +29,6 @@ if %ERRORLEVEL% NEQ 0 exit %ERRORLEVEL%
 
 ECHO Проверка проекта в EDT %date% - %time%
 
-call ring edt@%EDT_VERSION% workspace validate --workspace-location "%WORKSPACE%" --file "%VALIDATION_RESULT%" --project-list "%PROJECT_PATH%"
+call ring edt@%EDT_VERSION% workspace validate --workspace-location "%WORKSPACE%" --file "%EDT_VALIDATION_RESULT%" --project-list "%PROJECT_PATH%"
 
 ECHO Проверка завершена %date% - %time%
---
+++ test/convert.bat
@@ -0,0 +1,2 @@
+set LOGOS_CONFIG=logger.rootLogger=DEBUG
+oscript ../src/main.os c ./edt-result.out ./edt-json.json ./src
